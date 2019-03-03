cc8a7f110355dc7494a6a1b1cad9e153fe21c63d
Maxwell Todd <mtmaxtodd@gmail.com>
2019-03-03 15:54:09 -0600

    more practice with arrays

--- index.php
+++ index.php
@@ -14,11 +14,31 @@ echo "hello world";
 ?>
 
 <?php 
-$greeting = [ "hello ","world"
+$greeting = [ "<h1>hello</h1> ","<h6>world</h6>"
 ];
 for($i = 0; $i < count($greeting); $i++){
     echo $greeting[$i];
 }
+?>
+<?php 
+$php = 'Php';
+$python = 'Python';
+$languages = ["$php","$pyhton"];
+$knowIt = ['JavaScript','React','Redux','Node','SQL',];
+echo "Before today, I knew:";
+for($i = 0; $i < count($knowIt); $i++){
+   echo "<li>$knowIt[$i]</li>";
+}
+for($i = 0; $i < count($languages); $i++){
+    if($languages[$i]=='Php'){
+        array_push($knowIt, $languages[$i]);
+    }
+}
+echo "Now I know:";
+for($i = 0; $i < count($knowIt); $i++){
+   echo "<li>$knowIt[$i]</li>";
+}
+
 ?>
 </body>
 </html>
